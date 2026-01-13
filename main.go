212eabd416cd59eb0b7685ed2e51e8c505d0d9ab
VEDANT SHEKHAR KUGAONKAR <vedant.kugaonkar@gmail.com>
2026-01-13 11:31:31 +0530

    Fs

--- main.go
+++ main.go
@@ -2,31 +2,40 @@ package main
 
 import (
 	"flag"
+	"lawbook_video_chat/server"
 	"log"
 	"net/http"
 	"os"
-
-	"lawbook_video_chat/server"
 )
 
 func main() {
-	// Default address
+	// For local development: default 8080
 	addr := flag.String("addr", ":8080", "HTTP network address")
 	flag.Parse()
-
-	// Initialize room storage
 	server.AllRooms.Init()
+	go server.BroadCaster()
+
+	http.HandleFunc("/create", server.CreateRoomReqHandler)
+	http.HandleFunc("/join", server.JoinRoomReqHandler)
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// WebSocket signaling endpoint
-	http.HandleFunc("/ws/join", server.JoinRoomReqHandler)
-
-	infoLog.Printf("Starting signaling server on %s", *addr)
-
-	err := http.ListenAndServe(*addr, nil)
+	// IMPORTANT: Use the PORT environment variable on DigitalOcean
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	// local development fallback
+	// 	port = "8000"
+	// }
+	infoLog.Printf("Starting a server on %s", *addr)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		errorLog.Fatal(err)
+		errorLog.Println(err)
 	}
+
+	// infoLog.Printf("Starting a server on :%s", port)
+	// err := http.ListenAndServe(":"+port, nil)
+	// if err != nil {
+	// 	errorLog.Println(err)
+	// }
 }
